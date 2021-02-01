23ca11c37ce497c74b8ba876cb63a488ff43688e
Мартин <martin.samardziev@students.finki.ukim.mk>
2021-02-01 21:29:42 +0100

    added models, migrations, controllers, views and create functionality

---
+++ Rocky/Controllers/CategoryController.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Rocky.Data;
+using Rocky.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rocky.Controllers
+{
+    public class CategoryController : Controller
+    {
+        // section 2 - 9
+        private readonly ApplicationDbContext _db;
+        public CategoryController(ApplicationDbContext db)
+        {
+            _db = db;
+        }
+        public IActionResult Index()
+        {
+            IEnumerable<Category> objList = _db.Category; 
+            return View(objList);
+        }
+        // GET: /Category/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+        // POST - CREATE section 2 - 13
+        [HttpPost]
+        [ValidateAntiForgeryToken] // security token 
+        public IActionResult Create(Category obj)
+        {
+            _db.Category.Add(obj);
+            _db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+    }
+}
---
+++ Rocky/Models/Category.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rocky.Models
+{
+    public class Category
+    {
+        [Key] // because I have Id column, even if I write CategoryId, that will be detected as PK. But, if you want a PK, but the prop name doesnt contain Id, you should include [Key]
+        public int Id { get; set; }
+        public string Name { get; set; }
+        [DisplayName("Display Order")] // section 2 - 12 (whatever we write here, will be displayed in the view block, ex: DisplayOrder -> Display Order)
+        public int DisplayOrder { get; set; }
+    }
+}
