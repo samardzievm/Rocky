e51007eb5ca1744d7328407439d0167d61a161b5
Мартин <martin.samardziev@students.finki.ukim.mk>
2021-02-02 16:01:50 +0100

    added validations, edit and delete functionality, fontawesome to the buttons

--- Rocky/Controllers/CategoryController.cs
+++ Rocky/Controllers/CategoryController.cs
@@ -26,12 +26,78 @@ namespace Rocky.Controllers
         {
             return View();
         }
+
         // POST - CREATE section 2 - 13
         [HttpPost]
         [ValidateAntiForgeryToken] // security token 
         public IActionResult Create(Category obj)
         {
-            _db.Category.Add(obj);
+            if (ModelState.IsValid)
+            {
+                _db.Category.Add(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        // GET: EDIT
+        public IActionResult Edit(int? id)
+        {
+            if(id==null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var obj = _db.Category.Find(id);
+            if(obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+
+        // POST - EDIT section 3 - 5
+        [HttpPost]
+        [ValidateAntiForgeryToken] 
+        public IActionResult Edit(Category obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _db.Category.Update(obj);
+                _db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(obj);
+        }
+
+        // GET: DELETE
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var obj = _db.Category.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+
+        // POST - DELETE 
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePost(int? id)
+        {
+            var obj = _db.Category.Find(id);
+            if (obj == null)
+            {
+                return NotFound();
+            }
+            _db.Category.Remove(obj);
             _db.SaveChanges();
             return RedirectToAction("Index");
         }
