1746706703aaa1d2c23fed79868bcfb4b49cfac0
Мартин <martin.samardziev@students.finki.ukim.mk>
2021-02-05 16:38:11 +0100

    New homepage design, added partial view, added product details and added shopping card functionality

---
+++ Rocky/Models/ViewModels/DetailsVM.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rocky.Models.ViewModels
+{
+    public class DetailsVM
+    {
+        public DetailsVM()
+        {
+            // if you don't want to do this in the Controller, you can do that here
+            Product = new Product();
+        }
+        public Product Product { get; set; }
+        public bool ExistsInCart { get; set; }
+    }
+}
